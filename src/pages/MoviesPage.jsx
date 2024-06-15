6b9d2822d4bfcce83b04bb0fe5583ab94776c582
Dikhtiar Tania <you@example.com>
2024-06-15 13:00:42 +0300

    push new file

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,15 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { searchMoviesApi } from "../api/movies-api";
 
 const MoviesPage = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const getData = async () => {
-      const data = await searchMoviesApi();
+      try {
+        setLoading(true);
+        const data = await searchMoviesApi("cat");
+        setMovies(data);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false)
+       }
     };
     getData();
   }, []);
 
-  return <div>MoviesPage</div>;
+  return (
+    <div>
+      {movies.length > 0 &&
+        movies.map((movie) => {
+          <div key={movie.id}>
+            <h2><b>{movie.title}</b></h2>
+            <p>{movie.popularity}</p>
+          </div>;
+        })}
+    </div>
+  );
 };
 
 export default MoviesPage;
