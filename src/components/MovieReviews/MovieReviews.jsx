0394ec9f2028c96e20bb00ac7e0458983e8c482b
Dikhtiar Tania <you@example.com>
2024-06-18 10:25:13 +0300

    add review

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,6 +1,30 @@
-function MovieReviews() {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { fetchMovieReviews } from "../../api/movies-api";
+
+const MovieReviews = () => {
+  const {movieId} = useParams();
+  const [reviews, setReviews] = useState([]);
+  console.log('reviews', reviews)
+
+
+  useEffect(()=>{
+    fetchMovieReviews(movieId).then(setReviews)
+  }, [movieId]);
+
+
   return (
-    <div>Reviews</div>
+    <div>
+      <h2>Review</h2>
+      <ul>
+        {reviews.map(review => (
+          <li key={review.id}>
+            <h3>{review.author}</h3>
+            <p>{review.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
