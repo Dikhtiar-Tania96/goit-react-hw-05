10d5517c215edbaf9a4d70febe656528c17e10ba
Dikhtiar Tania <you@example.com>
2024-06-20 10:26:43 +0300

    fixed code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,52 +1,27 @@
-import { useEffect, useState } from "react";
-import { Link, useParams} from "react-router-dom";
-import { fetchTrendingMovies } from "../../api/movies-api";
-import Loader from "../../Loader/Loader";
-import css from './HomePage.module.css'
+
+import { useState, useEffect } from 'react';
+import MovieList from '../../components/MovieList/MovieList';
+import { fetchTrendingMovies } from '../../api/movies-api';
+
 
 const HomePage = () => {
-  const { movieId } = useParams();
   const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        setLoading(true);
-        setError(false);
-        const data = await fetchTrendingMovies("");
-        setMovies(data);
-      } catch (error) {
-        setError(true);
-        console.error('Error fetching trending movies:', error)
-      } finally {
-        setLoading(false);
-      }
-    };
-    getData();
+    fetchTrendingMovies().then(setMovies);
+    
   }, []);
 
+ 
   return (
-    <div>
-      <h1 className={css.titlePage}>Trending today🎬</h1>
-      {loading && <p><Loader/></p>}
-      {error && <p>Упс,помилка при завантаженні даних</p>}
-      <ul className={css.listFilms}>
-        {movies.map((movie) => {
-            return (
-              <li key={movie.id} className={css.itemFilms}>
-                <Link to={`/movies/${movie.id}`} className={css.linkFilms}>
-                <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
-                <h2 className={css.nameFilm}>{movie.title} </h2>               
-                <p className={css.nameFilmText}>{movie.release_date}</p>
-                </Link>
-              </li>
-            );
-          })}
-      </ul>
+    <div >
+      <h1>Popular Movies</h1>
+      <MovieList movies={movies} />
     </div>
   );
 };
 
 export default HomePage;
+
+
+
