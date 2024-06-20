c3ee90819d93b1dedcbd64f72d8cb836b3782743
Dikhtiar Tania <you@example.com>
2024-06-20 10:30:54 +0300

    push info

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import MovieList from '../../components/MovieList/MovieList';
 import { fetchTrendingMovies } from '../../api/movies-api';
+import css from './HomePage.module.css'
 
 
 const HomePage = () => {
@@ -15,8 +16,8 @@ const HomePage = () => {
  
   return (
     <div >
-      <h1>Popular Movies</h1>
-      <MovieList movies={movies} />
+    <h1 className={css.titlePage}>Trending films today🎬</h1>      
+    <MovieList movies={movies}/>
     </div>
   );
 };
